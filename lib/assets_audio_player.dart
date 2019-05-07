9516e60b5a92fb920af2bb07f900787ca66beba0
David Ross <david.ross@ceresti.com>
2019-05-07 12:59:58 -0700

    Add AndroidX support

--- lib/assets_audio_player.dart
+++ lib/assets_audio_player.dart
@@ -25,8 +25,7 @@ class AssetsAudioPlayer {
   final MethodChannel _channel = const MethodChannel('assets_audio_player');
 
   /// Then mediaplayer playing state (mutable)
-  final BehaviorSubject<bool> _isPlaying =
-      BehaviorSubject<bool>(seedValue: false);
+  final BehaviorSubject<bool> _isPlaying = BehaviorSubject<bool>()..add(false);
 
   /// Boolean observable representing the current mediaplayer playing state
   ///
@@ -61,8 +60,7 @@ class AssetsAudioPlayer {
   ValueObservable<PlayingAudio> get current => _current.stream;
 
   /// Called when the playing song finished (mutable)
-  final BehaviorSubject<bool> _finished =
-      BehaviorSubject<bool>(seedValue: false);
+  final BehaviorSubject<bool> _finished = BehaviorSubject<bool>()..add(false);
 
   /// Called when the current song has finished to play
   ///     _assetsAudioPlayer.finished.listen((finished){
@@ -72,8 +70,8 @@ class AssetsAudioPlayer {
   ValueObservable<bool> get finished => _isPlaying.stream;
 
   /// Then current playing song position (in seconds) (mutable)
-  final BehaviorSubject<Duration> _currentPosition =
-      BehaviorSubject<Duration>(seedValue: const Duration());
+  final BehaviorSubject<Duration> _currentPosition = BehaviorSubject<Duration>()
+    ..add(const Duration());
 
   /// Retrieve directly the current song position (in seconds)
   ///     final Duration position = _assetsAudioPlayer.currentPosition.value;
